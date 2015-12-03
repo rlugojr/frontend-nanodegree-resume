ce57e20baa2b6d453e86c5a29787c9608d20653f
Ray Lugo, Jr <rlugojr@gmail.com>
2015-12-03 03:43:34 -0500

    Bio Section Development
    Initial work on the development of the Bio section using the object
    variables to replace strings from the corresponding "helper.js" section
    in order to modify the DOM using jQuery at page load.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,37 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Ray Lugo, Jr.");
+ //$("#main").append("Ray Lugo, Jr.");
+
+/* var formattedName = HTMLheaderName.replace("%data%","Wyle E. Coyote");
+ var formattedRole = HTMLheaderRole.replace("%data%","Supra Genius");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
+
+var bio={
+	"name":"Wyle E. Coyote",
+	"role": "Supra Genius",
+	"contactMobile": "In the desert",
+	"pictureURL": "images/fry.jpg",
+	"welcomeMessage": "acme makes the best products!",
+	"skills":["Genius IQ","Expert Hunter", "Master Trap Designer"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+var formattedcontactMobile = HTMLmobile.replace("%data%",bio.contactMobile);
+var formattedBioPic = HTMLbioPic.replace("%data%",bio.pictureURL);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#topContacts").append(formattedcontactMobile);
+$("#header").prepend(formattedBioPic);
+$("#header").append(formattedWelcomeMessage);
+$("#header").append(HTMLskillsStart);
+for(item in bio.skills){
+	formattedSkills = HTMLskills.replace("%data%",bio.skills[item])
+	$("#header").append(formattedSkills);
+}
